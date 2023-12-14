c980537c5c0042bc83304611100d3f734387079b
Maisha Thasin <thasin.maisha@gmail.com>
2023-12-14 04:06:20 -0500

    add tailwind and get the minimal blog

--- generate_site.py
+++ generate_site.py
@@ -1,48 +1,54 @@
+
+
+
 import os
 import requests
-import sys; print(sys.path)
 import markdown2
 from jinja2 import Environment, FileSystemLoader
-from pprint import pprint
-from github import Github,Auth
-# Authentication is defined via github.Auth
-import marko
+from github import Github, Auth
 
-
-
-
-# Public Web Github
-
-
-# using an access token
+# Authentication and GitHub Initialization
 auth = Auth.Token("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")
-# Github username
-# pygithub object
-g = Github()
 g = Github(auth=auth)
-# get that user by username
-user = g.get_user()
 
+# Directory Definitions
+TEMPLATE_DIR = 'templates'
+OUTPUT_DIR = 'output'
+os.makedirs(OUTPUT_DIR, exist_ok=True)
 
-def create_blog_from_repo(repo_name):
-    repo = user.get_repo(repo_name)
-    print(repo)
-    contents = repo.get_contents("README.md")
-    #print(contents.decoded_content)
-    readme_md = requests.get(contents.download_url).text
-    readme_html = markdown2.markdown(contents.decoded_content)
-    print(readme_html)
-  
+def get_user_repositories(g):
+    user = g.get_user()
+    return user.get_repos()
 
-    env = Environment(loader=FileSystemLoader('templates'))
+def create_blog_content(g, repo_name):
+    try:
+        repo = g.get_user().get_repo(repo_name)
+        contents = repo.get_contents("README.md")
+        readme_html = markdown2.markdown(contents.decoded_content.decode('utf-8'))
+        return readme_html
+    except Exception as e:
+        print(f"Error processing {repo_name}: {e}")
+        return None
+
+def generate_blog_html(g, selected_repos):
+    repo_contents = {}
+    for repo in selected_repos:
+        content = create_blog_content(g, repo.name)
+        if content:
+            repo_contents[repo.name] = content
+
+    env = Environment(loader=FileSystemLoader(TEMPLATE_DIR))
     template = env.get_template('blog_template.html')
-    output_from_parsed_template = template.render(repo=repo, readme=readme_html)
+    output_from_parsed_template = template.render(repos=repo_contents)
 
-    output_dir = 'output'
-    os.makedirs(output_dir, exist_ok=True)
-    with open(os.path.join(output_dir, f'{repo_name}.html'), 'w') as file:
+    with open(os.path.join(OUTPUT_DIR, 'index.html'), 'w') as file:
         file.write(output_from_parsed_template)
 
-# Example usage
-# Replace 'repository_name' with the actual repository name
-create_blog_from_repo('mori.css')  
+repos = get_user_repositories(g)
+print("Select repositories to create blogs from (comma-separated):")
+for i, repo in enumerate(repos):
+    print(f"{i}: {repo.name}")
+
+selected_indexes = input()
+selected_repos = [repos[int(i)] for i in selected_indexes.split(',')]
+generate_blog_html(g, selected_repos)
